4378ebdbbb63c6ac263c0ae85968dc4d696427b6
fidacura <hello@fidacura.xyz>
2023-12-04 23:28:11 +0000

    git status

--- tests/test_password_strength.py
+++ tests/test_password_strength.py
@@ -1,1 +1,17 @@
 # cypherseed/cypherseed/test_password_strength.py
+
+import unittest
+from cypherseed import password_strength
+
+class TestPasswordStrength(unittest.TestCase):
+
+    def test_calculate_strength(self):
+        strength = password_strength.calculate_strength(4, 2048)
+        self.assertTrue(strength > 0)
+
+    def test_invalid_input(self):
+        with self.assertRaises(ValueError):
+            password_strength.calculate_strength(-1, 2048)
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_wordlists.py
+++ tests/test_wordlists.py
@@ -1,1 +1,19 @@
 # cypherseed/cypherseed/test_wordlists.py
+
+import unittest
+import os
+from cypherseed import wordlists
+
+class TestWordlists(unittest.TestCase):
+
+    def test_load_wordlist(self):
+        words = wordlists.load_wordlist('default')
+        self.assertIsInstance(words, list)
+        self.assertTrue(len(words) > 0)
+
+    def test_file_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            wordlists.load_wordlist('nonexistent')
+
+if __name__ == '__main__':
+    unittest.main()
